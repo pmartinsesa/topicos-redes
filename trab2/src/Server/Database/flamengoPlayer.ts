483169d2db2f363355386a545c3d9c4808e781d9
pmartinsesa <pmartinsesa@gmail.com>
2022-04-10 16:19:52 -0300

    feat: initialize flamengo route

---
+++ trab2/src/Server/Database/flamengoPlayer.ts
@@ -0,0 +1,17 @@
+export const FLAMENGO_PLAYERS = [
+    {
+        name: "De Arrascaeta",   
+        imageUrl: "https://pbs.twimg.com/media/ErGTryNXIAEJ6do?format=jpg&name=small",      
+        description: "Mago"            
+    },
+    {
+        name: "Bruno Henrique",   
+        imageUrl: "https://s2.glbimg.com/Mx6yWAGJhlG2N0YWFO0_WXXctco=/0x0:5760x3840/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2021/5/J/IiJ0VTStSppEjYQd7GuQ/z3rpafv5elzlrme9gjirlttgq3em3agkktlkivm2.jpeg",      
+        description: "Corre mais rápido que noticia ruim"            
+    },
+    {
+        name: "Gabriel Barbosa (Gabigol)",   
+        imageUrl: "https://media.gettyimages.com/photos/gabriel-barbosa-of-flamengo-scores-the-the-second-goal-of-his-team-picture-id1189601063?s=2048x2048",      
+        description: "Artilheiro da porra toda, gols perdidos e feitos."            
+    },
+]
